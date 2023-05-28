daf412016e5294d063083553c490a093d31ea3f3
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-05-28 23:34:34 +0530

    image add !(UI)

--- Asn1/course.php
+++ Asn1/course.php
@@ -6,12 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Course Info</title>
 </head>
+<style>
+    *,*::after,*::before{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+    }
+    h3,ul{
+        margin:0.5rem;
+    }
+    .cover-section{
+        width:100svw;
+        height:50svh;
+        position: relative;
+    }
+    .cover-section img{
+        position: absolute;
+        width:100%;
+        height:100%;
+    }
+    .info-wrapper{
+        position: relative;
+        width:100svw;
+        padding:1rem 1rem;
+        font-size:1.1rem;
+    }
+    .info-wrapper h1{
+        text-shadow:0 1px 5px #fff;
+        width:70%;
+        padding-bottom:0.5rem;
+        border-bottom:1px solid #000;
+    }
+</style>
 <body>
     <?php
         include 'dataRouting.php';
         $json=json_decode($info,true);
 
     ?>
+    <div class="cover-section" >
+    <img src=<?php echo $json[$_GET["index"]]["img"]; ?> alt="" loading="eager">
+
+    </div>
     <div class="info-wrapper">
         <h1>Department of 
             <?php echo $json[$_GET["index"]]["name"]; ?>
