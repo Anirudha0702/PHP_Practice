cc97bd6002dfd9b63a677eaeb2bd629d0163cea9
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-06-04 15:26:29 +0530

    update

--- Asn3/asn3.php
+++ Asn3/asn3.php
@@ -1,13 +1,18 @@
 <?php
-    $country_Capital=[];
-    $capital=array();
-    $length=2;
-    $len_Of_Each_Elmnt=2;
-    for($x=0;$x<$length;$x++){
-        $country_Capital[$x][0]=readline("Enter country name:  ");
-        $cntry_name=$country_Capital[$x][0];
-        $country_Capital[$x][1]=readline("Enter $cntry_name 's capital:  ");
-    }
+    $country_Capital=[
+        ['Italy','Rome'],
+        ['Belgium' ,'Brussels'],
+        ['Denmaek','Belgium Brussels'],
+        ['Finland','Helsinki'],
+        ['France','Paris'],
+        ['Germany','Berlin'],
+        ['Greece','Athens'],
+        ['Netherlands','Amsterdam'],
+        ['Portugal','Lisbon'],
+        ['Spain','Madrid']
+    ];
+    $length=count($country_Capital);
+    //Bubble Sort 
     for($x=0;$x<$length-1;$x++){
         for($y=0;$y<$length-1-$x;$y++){
             if(strcmp($country_Capital[$y+1][1],$country_Capital[$y][1])<= 0){
@@ -17,10 +22,10 @@
             }
         }
     }
-    echo("Country       Capital\n");
+    echo("Country        Capital\n");
     for($x=0;$x<$length;$x++){
         $country=$country_Capital[$x][0];
         $capital=$country_Capital[$x][1];
-        echo("$country     $capital\n");
+    echo("$country        $capital\n");
     }
 ?> 
