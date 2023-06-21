fbdc5b03c3c4b547036dd34db5993540da27f2f4
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-06-21 18:17:55 +0530

    date format update

--- Asn5/checkUser.php
+++ Asn5/checkUser.php
@@ -2,10 +2,10 @@
     $user_name=$_POST["user_name"];
     $user_password=$_POST["user_password"];
     if($user_name === "Anirudha D. Pradhan" && $user_password==="Robin@930"){
-        $date=date("D-m-y");
-        echo "<script>alert('hello $user_name you logged in at $date');</script>";
+        $date=date(DATE_COOKIE);
+        echo "<h2>hello $user_name you logged in at $date'</h2>;";
     }
     else{
-        echo "<script>alert('Not a valid user please try again');</script>";
+        echo "<h2>'Not a valid user please try again'</h2>";
     }
 ?>
